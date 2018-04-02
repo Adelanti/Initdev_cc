e3b76e290aa85f38d304e5cab903a5cb59d58f86
Adelanti <37774563+Adelanti@users.noreply.github.com>
2018-04-02 17:18:06 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,45 +1,11 @@
-
 #! /bin/bash
 
-chmod 777 $HOME/Bureau/initdev/main.sh 
 
-if  [ -z $1 ]  
-then
-
-     echo "Expected arguments, please check the help : initdev –help"
-     
-     exit 1
- 
-elif [ $1 = -help ] 
-then
-
-     echo "— Name : Afficher le nom de l’application et une petite description."
-     echo "— Syntax : Afficher la syntaxe d’utilisation de la commande initdev."
-     echo "— args : Lister les arguments et donner une description de chaque argument."     
-     echo "— author : Afficher le nom et l’email de l’auteur de l’application."     
-else
-
-
-    
-mkdir  $HOME/Bureau/$1
-cd $HOME/Bureau/$1
-touch  $HOME/Bureau/$1/main
-mkdir  $HOME/Bureau/$1/LICENSE     
-mkdir  $HOME/Bureau/$1/Makefile
-	       
-
-	 
-	if  [[ $2 = "" ]]   #sil que le nom de projrt je veux quil sorte 
-	then 
-    	exit 0
-     
-	else
-
-	cd $HOME
+		cd $HOME
 
 		if  [ -e .initdev ]
 		then
-		echo " fichier existe déja ! "
+		echo ""
 		else	
 		mkdir $(echo $HOME)/.initdev
 		cp -r $(echo $HOME)/Bureau/initdev/bin $HOME/.initdev/ 
@@ -49,120 +15,389 @@ mkdir  $HOME/Bureau/$1/Makefile
 		cp -r $(echo $HOME)/Bureau/initdev/makefiles $HOME/.initdev/
 		cp -r $(echo $HOME)/Bureau/initdev/main.sh $HOME/.initdev/
 		fi 
-	
-	elif  [[ $2 = -git ]]
-	then 
-					echo "You must set project type, please check the help : initdev –help"
-					exit 1
 
-	elif [[ $2 = -C ]] 
+if  [ -z $1 ]  
+then
+     echo "Expected arguments, please check the help : initdev –help"
+     
+     exit 1
+ 
+elif [ $1 = -help ] 
+then
+	if [[ $2 = "" ]] 
 	then
-					cp -r $(echo $HOME)/.initdev/sources/main.c $HOME/Bureau/$1/
-					
-	elif [[ $2 = -CPP ]]
-	then
- 					cp -r $(echo $HOME)/.initdev/sources/main.cpp $HOME/Bureau/$1/
-					
-	elif [[ $2 = -Python ]]
-	then
- 					cp -r $(echo $HOME)/.initdev/sources/main.Py $HOME/Bureau/$1/
-					
-	elif [[ $2 = -Latex ]]
-	then
-					cp -r $(echo $HOME)/.initdev/sources/main.tex $HOME/Bureau/$1/
-	else 
-					echo "incorect type of project"
-					exit 1
 
-	fi 
+     echo "— Name : "initdev" est un outil d'initialisation de projet de développement ."
+     echo "— Syntax : " 
+     echo " le programme doit se lancer par aumoins un argument qui porte le nom de projet   comme il est possible de le suivie par d'autre    arguments qui sert comme de pré-configurations de projet" 
+     echo "— args : "
+     echo "-git : sert a initialiser un dépot git "
+     echo "-C : permet créer un projet en langage C"
+     echo "-CPP : permet créer un projet en langage CPP"  
+     echo "-Python : permet créer un projet en langage Python" 
+     echo "-Latex : permet créer un projet de rédaction en Latex" 
+     echo "-BEAMER : permet créer un projet de présentation en BEAMER" 
+     echo "-GPL : permet de crée un projet avec un type de licence GPL"
+     echo "-MIT : permet de créer un projet avec un type de licence MIT "
+     echo "— author : "
+     echo " Name : GAMANE ADEL "
+     echo " Email : gamaneadel@gmail.com ."  
+   
+	else
+	echo "Argument After -help not allowed" 
+	exit 1
+	fi
+
+		
+
+else
+
+if [[ $2 = "" ]]
+then 
+mkdir  $HOME/Bureau/$1
+touch  $HOME/Bureau/$1/main
+mkdir  $HOME/Bureau/$1/LICENSE
+mkdir  $HOME/Bureau/$1/Makefile	
+exit 0
+fi 
 
 
-		if [[ $3 = "" ]] 
-		then
-			exit 0
 
-                elif [[ $3 = -git ]]
+
+#il interdit de taper plus qu'un type de licence ou de projet
+
+  
+
+if [[ $2 = -C ]] && [[ $3 = -CPP || $3 = -BEAMER || $3 = -Latex || $3 = -Python || $4 = -CPP || $4 = -BEAMER || $4 = -Latex || $4 = -Python ]]
+then 
+echo " Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $2 = -CPP ]] && [[ $3 = -C || $3 = -BEAMER || $3 = -Latex || $3 = -Python || $4 = -C || $4 = -BEAMER || $4 = -Latex || $4 = -Python ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1 
+elif [[ $2 = -Python ]] && [[ $3 = -CPP || $3 = -BEAMER || $3 = -Latex || $3 = -C || $4 = -CPP || $4 = -BEAMER || $4 = -Latex || $4 = -C ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $2 = -BEAMER ]] && [[ $3 = -CPP || $3 = -C || $3 = -Latex || $3 = -Python || $4 = -CPP || $4 = -C || $4 = -Latex || $4 = -Python ]]
+then 
+echo " Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $2 = -Latex ]] && [[ $3 = -CPP || $3 = -BEAMER || $3 = -C || $3 = -Python || $4 = -CPP || $4 = -BEAMER || $4 = -C || $4 = -Python ]]
+then 
+echo " Similar Type Of Argument, please check the help : initdev –help" 
+exit 1
+echo ""
+fi
+
+if [[ $3 = -C ]] && [[ $2 = -CPP || $2 = -BEAMER || $2 = -Latex || $2 = -Python || $4 = -CPP || $4 = -BEAMER || $4 = -Latex || $4 = -Python ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $3 = -CPP ]] && [[ $2 = -C || $2 = -BEAMER || $2 = -Latex || $2 = -Python || $4 = -C || $4 = -BEAMER || $4 = -Latex || $4 = -Python ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1 
+elif [[ $3 = -Python ]] && [[ $2 = -CPP || $2 = -BEAMER || $2 = -Latex || $2 = -C || $4 = -CPP || $4 = -BEAMER || $4 = -Latex || $4 = -C ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $3 = -BEAMER ]] && [[ $2 = -CPP || $2 = -C || $2 = -Latex || $2 = -Python || $4 = -CPP || $4 = -C || $4 = -Latex || $4 = -Python ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $3 = -Latex ]] && [[ $2 = -CPP || $2 = -BEAMER || $2 = -C || $2 = -Python || $4 = -CPP || $4 = -BEAMER || $4 = -C || $4 = -Python ]]
+then 
+echo " Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+echo ""
+fi
+
+if [[ $4 = -C ]] && [[ $3 = -CPP || $3 = -BEAMER || $3 = -Latex || $3 = -Python || $2 = -CPP || $2 = -BEAMER || $2 = -Latex || $2 = -Python ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $4 = -CPP ]] && [[ $3 = -C || $3 = -BEAMER || $3 = -Latex || $3 = -Python || $2 = -C || $2 = -BEAMER || $2 = -Latex || $2 = -Python ]]
+then 
+echo " Similar Type Of Argument, please check the help : initdev –help" 
+exit 1 
+elif [[ $4 = -Python ]] && [[ $3 = -CPP || $3 = -BEAMER || $3 = -Latex || $3 = -C || $2 = -CPP || $2 = -BEAMER || $2 = -Latex || $2 = -C ]]
+then 
+echo " Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $4 = -BEAMER ]] && [[ $3 = -CPP || $3 = -C || $3 = -Latex || $3 = -Python || $2 = -CPP || $2 = -C || $2 = -Latex || $2 = -Python ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $4 = -Latex ]] && [[ $3 = -CPP || $3 = -BEAMER || $3 = -C || $3 = -Python || $2 = -CPP || $2 = -BEAMER || $2 = -C || $2 = -Python ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+echo ""
+fi
+
+if [[ $2 = -GPL ]] && [[ $3 = -MIT || $4 = -MIT ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $3 = -GPL ]] && [[ $2 = -MIT || $4 = -MIT ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1 
+elif [[ $4 = -GPL ]] && [[ $3 = -MIT || $2 = -MIT ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $2 = -MIT ]] && [[ $3 = -GPL || $4 = -GPL ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $3 = -MIT ]] && [[ $2 = -GPL || $4 = -GPL ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1
+elif [[ $4 = -MIT ]] && [[ $3 = -GPL || $2 = -GPL || $3 = -Latex || $3 = -Python || $4 = -C || $4 = -BEAMER || $4 = -Latex || $4 = -Python ]]
+then 
+echo "  Similar Type Of Argument, please check the help : initdev –help " 
+exit 1 
+else
+echo ""
+fi
+
+
+#fin de test1 
+
+
+
+
+#interdire de taper un argument non defini 
+
+
+	    if [[ $2 != -CPP && $2 != -Latex && $2 != -BEAMER && $2 != -Python && $2 != -C && $2 != -MIT && $2 != -GPL && $2 != -git && $2 != "" ]]
+	    then
+	    	echo "Uknown arguments, please check the help : initdev –help"
+		exit 1
+	    elif [[  $3 != -CPP && $3 != -Latex && $3 != -BEAMER && $3 != -Python && $3 != -C && $3 != -MIT && $3 != -GPL && $3 != -git && $3 != "" ]]
+	    then
+		echo "Uknown arguments, please check the help : initdev –help"
+		exit 1
+	    elif [[ $4 != -CPP && $4 != -Latex && $4 != -BEAMER && $4 != -Python && $4 != -C && $4 != -MIT && $4 != -GPL && $4 != -git && $4 != "" ]]	
+	    then
+		echo "Uknown arguments, please check the help : initdev –help"
+		exit 1
+
+
+
+	     
+
+	     else
+
+		if [[ $2 != $3 && $2 != $4 ]]
 		then 
-					mkdir $HOME/Bureau/$1/gitignores
-
-					if [[ $2 = -C ]] 
-					then
-					cp -r $(echo $HOME)/.initdev/gitignores/c $HOME/Bureau/$1/gitignores/
-					
-					elif [[ $2 = -CPP ]]
-					then
- 					cp -r $(echo $HOME)/.initdev/gitignores/cpp $HOME/Bureau/$1/gitignores/
-
-					elif [[ $2 = -Python ]]
-					then
- 					cp -r $(echo $HOME)/.initdev/gitignores/py $HOME/Bureau/$1/gitignores/
-					
-					elif [[ $2 = -Latex ]]
-					then
-					cp -r $(echo $HOME)/.initdev/gitignores/tex $HOME/Bureau/$1/gitignores/
-					else
- 
-					fi
-git init 
-		exit 0
 			
+					
+					
+
+	             if [[ $2 = -git || $3 = -git || $4 = -git ]] 
+	             then 
 			
-		elif [[ $3 = -GPL ]] 
-		then
-			cp -r $(echo $HOME)/.initdev/licenses/GPL $HOME/Bureau/$1/LICENSE/
- 
-		elif [[ $3 = -MIT ]] 
-		then
-			cp -r $(echo $HOME)/.initdev/licenses/MIT $(echo $HOME)/Bureau/$1/LICENSE/
-			 
-		else 
 
-			echo " incorect type of license "
-			exit 1 
-		fi
+		        if [[ $2 = -C || $3 = -C || $4 = -C ]] 
+		        then 
 
- 
-
-			if [[ $4 = "" ]]
-			then 
-					exit 0
-
-			elif [[ $4 = -git ]]
+		    	if  [ -e $HOME/Bureau/$1 ]
 			then
- 
-					mkdir $HOME/Bureau/$1/gitignores
+	    		cp -r $(echo $HOME)/.initdev/sources/main.c $HOME/Bureau/$1/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile
+			    mkdir $HOME/Bureau/$1/gitignores	
+	    		cp -r $(echo $HOME)/.initdev/sources/main.c $HOME/Bureau/$1/	    
+			fi
+			cp -r $(echo $HOME)/.initdev/gitignores/c $HOME/Bureau/$1/gitignores/
+git init
+		        elif [[ $2 = -CPP || $3 = -CPP || $4 = -CPP ]]   
+		        then
+		    
+			if  [ -e $HOME/Bureau/$1 ]
+			then
+	    		cp -r $(echo $HOME)/.initdev/sources/main.cpp $HOME/Bureau/$1/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile
+			    mkdir $HOME/Bureau/$1/gitignores	
+	    		cp -r $(echo $HOME)/.initdev/sources/main.cpp $HOME/Bureau/$1/	    
+			fi
+			cp -r $(echo $HOME)/.initdev/gitignores/cpp $HOME/Bureau/$1/gitignores/
+git init
+		        elif [[ $2 = -Python || $3 = -Python || $4 = -Python ]]	 
+		        then
+		    
+			if  [ -e $HOME/Bureau/$1 ]
+			then
+	    		cp -r $(echo $HOME)/.initdev/sources/main.py $HOME/Bureau/$1/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile
+			    mkdir $HOME/Bureau/$1/gitignores	
+	    		cp -r $(echo $HOME)/.initdev/sources/main.py $HOME/Bureau/$1/	    
+			fi
+		        cp -r $(echo $HOME)/.initdev/gitignores/python $HOME/Bureau/$1/gitignores/
+git init 
+		        elif [[ $2 = -Latex || $3 = -Latex || $4 = -Latex ]]    
+		        then
+			if  [ -e $HOME/Bureau/$1 ]
+			then
+	    		cp -r $(echo $HOME)/.initdev/sources/latexmin.tex $HOME/Bureau/$1/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile
+			    mkdir $HOME/Bureau/$1/gitignores	
+	    		cp -r $(echo $HOME)/.initdev/sources/latexmin.tex $HOME/Bureau/$1/	    
+			fi
+		        cp -r $(echo $HOME)/.initdev/gitignores/tex $HOME/Bureau/$1/gitignores/
+git init
+		        else
+		        echo "You Must Set a Project Type, please check the help : initdev –help" 
+		        exit 1
 
+		        fi
+	            fi
+          
+	 
+			
+	   
 
+	  
 
-	                                if [[ $2 = -C ]] 
-					then
-					cp -r $(echo $HOME)/.initdev/gitignores/c $HOME/Bureau/$1/gitignores/
-					
-					elif [[ $2 = -CPP ]]
-					then
- 					cp -r $(echo $HOME)/.initdev/gitignores/cpp $HOME/Bureau/$1/gitignores/
+	if [[ $2 = -Python || $3 = -Python || $4 = -Python ]] 
+        then
+	    
+	    		if  [ -e $HOME/Bureau/$1 ]
+			then
+	    		cp -r $(echo $HOME)/.initdev/sources/main.py $HOME/Bureau/$1/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile	
+	    		cp -r $(echo $HOME)/.initdev/sources/main.py $HOME/Bureau/$1/	    
+			fi	
+	
+	elif [[ $2 = -CPP || $3 = -CPP || $3 = -CPP ]] 
+	then
+	   
+	    		if  [ -e $HOME/Bureau/$1 ]
+			then
+	    		cp -r $(echo $HOME)/.initdev/sources/main.cpp $HOME/Bureau/$1/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile	
+	    		cp -r $(echo $HOME)/.initdev/sources/main.cpp $HOME/Bureau/$1/	    
+			fi    
+	
 
-					elif [[ $2 = -Python ]]
-					then
- 					cp -r $(echo $HOME)/.initdev/gitignores/py $HOME/Bureau/$1/gitignores/
-					
-					elif [[ $2 = -Latex ]]
-					then
-					cp -r $(echo $HOME)/.initdev/gitignores/tex $HOME/Bureau/$1/gitignores/
+	elif [[ $2 = -Latex || $3 = -Latex || $4 = -Latex ]] 
+        then
+	    
+	                if  [ -e $HOME/Bureau/$1 ]
+			then
+	    		cp -r $(echo $HOME)/.initdev/sources/latexmin.tex $HOME/Bureau/$1/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile	
+	    		cp -r $(echo $HOME)/.initdev/sources/latexmin.tex $HOME/Bureau/$1/	    
+			fi
+	
+	elif [[ $2 = -BEAMER || $3 = -BEAMER || $4 = -BEAMER ]] 
+	then
+	    
+	   		if  [ -e $HOME/Bureau/$1 ]
+			then
+	    		cp -r $(echo $HOME)/.initdev/sources/beamer.tex $HOME/Bureau/$1/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile	
+	    		cp -r $(echo $HOME)/.initdev/sources/beamer.tex $HOME/Bureau/$1/	    
+			fi
+	
+	elif [[ $2 = -C || $3 = -C || $4 = -C ]] 
+        then 
+	    
+	   		if  [ -e $HOME/Bureau/$1 ]
+			then
+	    		cp -r $(echo $HOME)/.initdev/sources/main.c $HOME/Bureau/$1/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile	
+	    		cp -r $(echo $HOME)/.initdev/sources/main.c $HOME/Bureau/$1/	    
+			fi
+	
 
-					else
-					echo "" 
-					fi
+	else
+	 
+	echo ""
+	
+	fi
 
-git init					
-					exit 0
-					else
-					echo " u must set -git " 
-					exit 1
-
+	if [[ $2 = -GPL || $3 = -GPL || $4 = -GPL ]] 
+	  then
+			if  [ -e $HOME/Bureau/$1 ]
+			then
+	    cp -r $(echo $HOME)/.initdev/licenses/GPL $HOME/Bureau/$1/LICENSE/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    touch  $HOME/Bureau/$1/main
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile	
+	    cp -r $(echo $HOME)/.initdev/licenses/GPL $HOME/Bureau/$1/LICENSE/	    
+			fi
+		
+	  elif [[ $2 = -MIT || $3 = -MIT || $4 = -MIT ]] 
+	  then
+	    		if  [ -e $HOME/Bureau/$1 ]
+			then
+	    cp -r $(echo $HOME)/.initdev/licenses/MIT $HOME/Bureau/$1/LICENSE/
+			else 
+			    mkdir  $HOME/Bureau/$1
+			    touch  $HOME/Bureau/$1/main
+			    mkdir  $HOME/Bureau/$1/LICENSE
+			    mkdir  $HOME/Bureau/$1/Makefile	
+	    cp -r $(echo $HOME)/.initdev/licenses/MIT $HOME/Bureau/$1/LICENSE/	    
 			fi
 
-exit 0		
-	
-	
-fi 
+
+	  else
+	  echo ""
+
+	  fi	
+
+
+			
+			else
+			echo "Similar Argument Not Allowed, please check the help : initdev –help " 
+			exit 1
+				
+		
+                 fi     
+
+
+
+
+	fi
+
+
+
+fi
+
+
+
+
