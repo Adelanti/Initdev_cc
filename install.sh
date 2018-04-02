89490a173770debe21b91e101c0cdc2e74cb0f0a
Adelanti <37774563+Adelanti@users.noreply.github.com>
2018-04-02 17:39:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@ Ouvrir un terminal
 taper : chmod +x ( le chemin fichier script "main" ) # pour lui accorder les droits d'exécuions . 
 ensuite taper : cd $HOME 
                 gedit .bashrc
-ajouter cette ligne avec les alias  : alias = "le chemin du main "               
+ajouter cette ligne avec les alias  : alias  initdev = "le chemin du script main "               
 
 et copier cette ligne a la fin du fichier :
  PATH=$PATH+'lien du fichier script' sur le fichier  .bashrc
  cntlr + c et terminer 
- 
-PS "je a la derniere étape je vais le rediger correctement " 
+  
