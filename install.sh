37ecab30d6a934b4f5de284e8971d11a12f4cea9
Adelanti <37774563+Adelanti@users.noreply.github.com>
2018-04-06 00:56:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ ajouter cette ligne avec les alias  : alias  initdev = "le chemin du script main
 
 et copier cette ligne a la fin du fichier :
  PATH=$PATH+'lien du fichier script' sur le fichier  .bashrc
- cntlr + c et terminer 
+ sauvegarder
+ puis clicker sur cntlr + c et terminer .
   
